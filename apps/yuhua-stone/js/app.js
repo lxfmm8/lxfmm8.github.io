c5268df894ebea7c0fda411648dbfb6e163fd7ff
Timmi Lin <linxiaofeng007@yahoo.com>
2017-03-12 22:38:16 -0400

    yuhua-site

---
+++ apps/yuhua-stone/js/app.js
@@ -0,0 +1,48 @@
+var htmlBody = document.querySelector("html");
+var buttonHome = document.querySelector(".btnHome");
+var buttonHistory = document.querySelector(".btnHistory");
+var buttonDiscern = document.querySelector(".btnDiscern");
+var buttonStore = document.querySelector(".btnStore");
+var pageTitleChinese = document.querySelector("#pageTitleCN");
+var pageTitleEnglish = document.querySelector("#pageTitleEN");
+var pageNum = document.querySelector("#pageNumber");
+
+
+buttonClear = function() {
+	htmlBody.classList.remove("selectedHome");
+	htmlBody.classList.remove("selectedHistory");
+	htmlBody.classList.remove("selectedDiscern");
+	htmlBody.classList.remove("selectedStore");
+};
+
+buttonHome.addEventListener ("click", function(){
+	buttonClear();
+	htmlBody.classList.add("selectedHome");
+	pageTitleCN.textContent = "白雨花";
+	pageTitleEN.textContent = "White Yuhua";
+	pageNum.textContent = "01";
+});
+
+buttonHistory.addEventListener ("click", function(){
+	buttonClear();
+	htmlBody.classList.add("selectedHistory");
+	pageTitleCN.textContent = "历史";
+	pageTitleEN.textContent = "Yuhua History";
+	pageNum.textContent = "02";
+});
+
+buttonDiscern.addEventListener ("click", function(){
+	buttonClear();
+	htmlBody.classList.add("selectedDiscern");
+	pageTitleCN.textContent = "甄别";
+	pageTitleEN.textContent = "Yuhua Discern";
+	pageNum.textContent = "03";
+});
+
+buttonStore.addEventListener ("click", function(){
+	buttonClear();
+	htmlBody.classList.add("selectedStore");
+	pageTitleCN.textContent = "商店";
+	pageTitleEN.textContent = "Yuhua Store";
+	pageNum.textContent = "04";
+});
